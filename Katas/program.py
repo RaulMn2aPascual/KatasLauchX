3d304243622730c83ccd025b4df346aef63a1867
unknown <launchx00920@innovaccion.mx>
2022-02-21 20:27:47 -0600

    se agrega la carpeta con los programas

---
+++ Katas/program.py
@@ -0,0 +1,23 @@
+
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+
+planetas_en_el_sistema_solar = 8 # int, plutón era considerado un planeta pero ya es muy pequeño
+distancia_a_alfa_centauri = 4.367 # float, años luz
+puede_despegar = True
+transbordador_que_aterrizo_en_la_luna = "Apollo 11" #string
+# Declaramos la variable
+distancia_a_alfa_centauri = 4.367
+
+# Descubrimos su tipo de dato
+type(distancia_a_alfa_centauri)
+
+# Importamos la biblioteca 
+from datetime import date
+
+# Obtenemos la fecha de hoy
+date.today()
+
+# Mostramos la fecha en la consola
+print(date.today())
